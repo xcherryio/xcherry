01272292ab2cc89fdff7bbf4bb412e2d975a9551
Quanzheng Long <qlong@indeed.com>
2023-09-26 22:01:26 -0700

    fix hc

--- integTests/main_test.go
+++ integTests/main_test.go
@@ -1,9 +1,15 @@
 package integTests
 
 import (
+	"flag"
 	"testing"
 )
 
+var postgresIntegTest = flag.Bool("postgres", false, "run integ test against Postgres")
+
 func TestMain(m *testing.M) {
-	
+	flag.Parse()
+	if *postgresIntegTest {
+		
+	}
 }
