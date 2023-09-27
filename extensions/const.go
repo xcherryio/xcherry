eb5deb7645301350150a5a88e8c1869e98b4453d
Quanzheng Long <qlong@indeed.com>
2023-09-27 10:41:19 -0700

    Setup schema for Postgres

--- extensions/const.go
+++ extensions/const.go
@@ -1,23 +1,14 @@
 package extensions
 
 const (
-	// CLIOptEndpoint is the cli option for endpoint
-	CLIOptEndpoint = "endpoint"
-	// CLIOptPort is the cli option for port
-	CLIOptPort = "port"
-	// CLIOptUser is the cli option for user
-	CLIOptUser = "user"
-	// CLIOptPassword is the cli option for password
-	CLIOptPassword = "password"
-	CLIOptDatabase = "database"
-
-	// CLIFlagEndpoint is the cli flag for endpoint
-	CLIFlagEndpoint = CLIOptEndpoint + ", ep"
-	// CLIFlagPort is the cli flag for port
-	CLIFlagPort = CLIOptPort + ", p"
-	// CLIFlagUser is the cli flag for user
-	CLIFlagUser = CLIOptUser + ", u"
-	// CLIFlagPassword is the cli flag for password
-	CLIFlagPassword = CLIOptPassword + ", pw"
-	CLIFlagDatabase = CLIOptDatabase + ", db"
+	// CLIFlagEndpoint is the cli option for endpoint
+	CLIFlagEndpoint = "endpoint"
+	// CLIFlagPort is the cli option for port
+	CLIFlagPort = "port"
+	// CLIFlagUser is the cli option for user
+	CLIFlagUser = "user"
+	// CLIFlagPassword is the cli option for password
+	CLIFlagPassword = "password"
+	CLIFlagDatabase = "database"
+	CLIFlagFile     = "file"
 )
--- integTests/main_test.go
+++ integTests/main_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/xdblab/xdb/config"
 	"github.com/xdblab/xdb/extensions"
 	"github.com/xdblab/xdb/extensions/postgres"
+	"github.com/xdblab/xdb/extensions/postgres/postgrestool"
 	"testing"
 	"time"
 )
@@ -13,22 +14,31 @@ import (
 var testDatabaseName = fmt.Sprintf("tst%v", time.Now().UnixNano())
 
 func TestMain(m *testing.M) {
-	fmt.Println("start running integ test")
-
 	flag.Parse()
+	fmt.Printf("start running integ test, using database %v, postgres:%v \n", testDatabaseName, *postgresIntegTest)
+	var resultCode int
+
 	if *postgresIntegTest {
 		sqlConfig := &config.SQL{
-			ConnectAddr:     "0.0.0.0:5432",
-			User:            "xdb",
-			Password:        "xdbxdb",
+			ConnectAddr:     fmt.Sprintf("%v:%v", postgrestool.DefaultEndpoint, postgrestool.DefaultPort),
+			User:            postgrestool.DefaultUserName,
+			Password:        postgrestool.DefaultPassword,
 			DBExtensionName: postgres.ExtensionName,
+			DatabaseName:    testDatabaseName,
 		}
-		err := extensions.CreateDatabase(sqlConfig, testDatabaseName)
+		err := extensions.CreateDatabase(*sqlConfig, testDatabaseName)
+		if err != nil {
+			panic(err)
+		}
+		err = extensions.SetupSchema(sqlConfig, "../"+postgrestool.DefaultSchemaFilePath)
 		if err != nil {
 			panic(err)
 		}
 		defer func() {
-			err := extensions.DropDatabase(sqlConfig, testDatabaseName)
+			if *keepDatabaseForDebugWhenTestFails && resultCode != 0 {
+				return
+			}
+			err := extensions.DropDatabase(*sqlConfig, testDatabaseName)
 			if err != nil {
 				panic(err)
 			}
@@ -37,6 +47,6 @@ func TestMain(m *testing.M) {
 		fmt.Println("test database created:", testDatabaseName)
 	}
 
-	code := m.Run()
-	fmt.Println("finished running integ test with status code", code)
+	resultCode = m.Run()
+	fmt.Println("finished running integ test with status code", resultCode)
 }
