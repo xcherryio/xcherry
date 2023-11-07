96b126d7c3acd06951d29d1928abf8762e77fe39
Quanzheng Long <qlong@indeed.com>
2023-11-07 15:16:49 -0800

    Change to BSL license

--- persistence/data_models_json_fields.go
+++ persistence/data_models_json_fields.go
@@ -1,15 +1,5 @@
-// Copyright 2023 XDBLab organization
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Copyright (c) XDBLab
+// SPDX-License-Identifier: BUSL-1.1
 
 package persistence
 
@@ -138,7 +128,8 @@ func NewStateExecutionLocalQueuesFromBytes(bytes []byte) (StateExecutionLocalQue
 }
 
 func (s *StateExecutionLocalQueuesJson) AddNewLocalQueueCommands(
-	stateExecutionId StateExecutionId, localQueueCommands []xdbapi.LocalQueueCommand) {
+	stateExecutionId StateExecutionId, localQueueCommands []xdbapi.LocalQueueCommand,
+) {
 	stateExecutionIdKey := stateExecutionId.GetStateExecutionId()
 	_, ok := s.StateToLocalQueueCommandsMap[stateExecutionIdKey]
 	if !ok {
@@ -224,8 +215,10 @@ func (s *StateExecutionLocalQueuesJson) AddMessageAndTryConsume(message LocalQue
 // and returns:
 //
 // { 0: [(id_1_1, false)], 1: [(id_2_1, false), (id_2_2, false)] }
-func (s *StateExecutionLocalQueuesJson) TryConsumeForStateExecution(stateExecutionId StateExecutionId,
-	commandWaitingType xdbapi.CommandWaitingType) map[int][]InternalLocalQueueMessage {
+func (s *StateExecutionLocalQueuesJson) TryConsumeForStateExecution(
+	stateExecutionId StateExecutionId,
+	commandWaitingType xdbapi.CommandWaitingType,
+) map[int][]InternalLocalQueueMessage {
 	stateExecutionIdKey := stateExecutionId.GetStateExecutionId()
 
 	// command_index: {...}
